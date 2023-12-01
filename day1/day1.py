d212bb57f68a8fe7b9efa441fd98eff4963baefd
rezaarezvan <rezvanr2378@gmail.com>
2023-12-01 18:44:13 +0100

    w/e

--- day1/day1.py
+++ day1/day1.py
@@ -1,36 +1,26 @@
 inp = [l.strip() for l in open('input')]
 nums = []
-
 m = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
      "six": 6, "seven": 7, "eight": 8, "nine": 9}
-
-
-# Part 1
-for word in inp:
-    res = ""
-    for char in word:
+for w in inp:
+    r = ""
+    for char in w:
         if char.isdigit():
-            res += char
+            r += char
 
-    res = res[0] + res[-1]
-    nums.append(int(res))
-
+    r = r[0] + r[-1]
+    nums.append(int(r))
 print(sum(nums))
-
-# Part 2
 nums = []
-for word in inp:
-    res = ""
+for w in inp:
+    r = ""
     for num in m:
-        if num in word:
-            word = word.replace(num, num + str(m[num]) + num)
-    for char in word:
+        if num in w:
+            w = w.replace(num, num + str(m[num]) + num)
+    for char in w:
         if char.isdigit():
-            res += char
-
-    res = "".join(res)
-    res = res[0] + res[-1]
-    nums.append(int(res))
-
-
+            r += char
+    r = "".join(r)
+    r = r[0] + r[-1]
+    nums.append(int(r))
 print(sum(nums))
